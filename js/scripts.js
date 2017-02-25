c81a83e895cca673445a8c38b00e5735bfe0a695
Tyson Stuart <stuarttyson.ts@gmail.com>
2017-02-25 14:04:26 -0800

    add the final touches along with readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#inputs").submit(function(event) {
-    
+
     var name = $("#name").val();
     var partysize = parseInt($("#partysize").val());
     var traveltime = parseInt($("#traveltime").val());
@@ -14,18 +14,27 @@ $(document).ready(function() {
       }  else if (partysize + traveltime + budget <= 2009) {
         $("#output").text(name + " pack the bugspray and check out these popular backpacking destinations!")
         $("#backpacker").show();
+        $("#backpackerdescript").show();
         $("#airbnb").hide();
+        $("#airbnbdescript").hide();
         $("#cruiseship").hide();
+        $("#cruisedescript").hide();
       }  else if (partysize + traveltime + budget < 4000 && partysize + traveltime + budget >= 2010) {
         $("#output").text(name + " , looks like you'd enjoy getting out and exploring a new city!");
         $("#backpacker").hide();
+        $("#backpackerdescript").hide();
         $("#airbnb").show();
+        $("#airbnbdescript").show();
         $("#cruiseship").hide();
+        $("#cruisedescript").hide();
       }  else if (partysize + traveltime + budget >= 4001) {
         $("#output").text(name + " , sit back and relax while enjoying a relaxing cruise to a new destination.")
         $("#backpacker").hide();
+        $("#backpackerdescript").hide();
         $("#airbnb").hide();
+        $("#airbnbdescript").hide()
         $("#cruiseship").show();
+        $("#cruisedescript").show();
       }
     event.preventDefault();
    });
---
+++ readme.md
@@ -0,0 +1,38 @@
+# Vacation Suggester
+
+#### Site used to help determine best method of vacation, 2/25/2017
+
+#### By Tyson Stuart
+
+## Description
+
+This is a simple from that asks the user questions about predetermined parameters. Once the necessary fields have been filled out the user can submit the form and a vacation method will be produced.  
+
+## Setup/Installation Requirements
+
+* Clone this repository.
+* Launch index.html file in a web browser
+* Input answers into required fields
+* Click submit button
+
+## Known Bugs
+
+There are no known bugs
+
+## Support and contact details
+
+Should you run into any bugs within this code please contact developer at stuarttyson.ts@gmail.com
+
+## Technologies Used
+
+HTML
+CSS
+Bootstrap
+Jquery
+Javascript
+
+### License
+
+This software is licensed under the MIT License.
+
+Copyright (c) 2017 **Tyson Stuart**
